a365827bdd79b2c083931902a4ebcd04368a6716
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2025-04-30 20:31:11 +0330

    reimplement CustomUser model and makemigrations afresh

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 
 class CustomUser(AbstractUser):
-    pass
+    phone_number = models.CharField(
+        max_length=11,
+        validators=[
+            RegexValidator(
+                regex=r"^\d{11}$",
+                message="Phone number must be exactly 11 digits.",
+                code="invalid_phone_number",
+            ),
+        ],
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    telegram_chat_id = models.CharField(
+        max_length=32,
+        unique=True,
+        null=True,
+        blank=True,
+        verbose_name="چت آی دی تلگرام",
+    )
